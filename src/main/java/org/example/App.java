9cdc646013e1b9b73df722be8ac2bcf25e0ea759
Nik <tetst>
2021-07-17 01:10:29 +0300

    Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,26 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+@ComponentScan(basePackages = {"Bean_test"})
+public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ){
+        SpringApplication.run(WebMvcApp.class, args);
+    }
     {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainConfig.class);
-        //ClassicMusic magic = context.getBean(ClassicMusic.class);
+        ClassicMusic magic = context.getBean(ClassicMusic.class);
 
         MagicSpringClass magicSpringClass = new MagicSpringClass(magic);
         magicSpringClass.say();
--- src/main/java/org/example/MainConfig.java
+++ src/main/java/org/example/MainConfig.java
@@ -2,11 +2,13 @@ package org.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MainConfig {
 
     @Bean
+    @Scope("singleton")
     public ClassicMusic classicMusic(){
         return new ClassicMusic();
     }
